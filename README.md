b7c1c242433b621f950f77c04f4397c89b9bfd74
DavFount <47730893+DavFount@users.noreply.github.com>
2023-05-28 20:06:55 -0700

    Converted UI to Vue. Added a currency selection. Converted the blip colors to a table lookup. Converted events to NetEvents and refactored to inline closures.

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Wagons
 
 #### Description
+
 This is a wagons script for RedM servers using the [VORP framework](https://github.com/VORPCORE). Wagons can be bought and sold through shops. There are 7 shops configured, more shop locations may be added using the `config.lua` file.
 
 #### Features
+
 - Buy and sell wagons through the wagon shops
 - Cash or gold may be used for payments in the menu
 - Individual inventory for owned wagons
@@ -17,21 +19,29 @@ This is a wagons script for RedM servers using the [VORP framework](https://gith
 - More to come!
 
 #### Commands
-- Command `/wagonRespawn` to bypass distance check and respawn wagon
+
 - Command `/wagonReturn` to return wagon when away from a shop
 
 #### Configuration
+
 Settings can be changed in the `config.lua` file. Here is an example of one shop:
+
 ```lua
+    -- Which currancy type is allowed?
+    -- 0 = Cash Only
+    -- 1 = Gold Only
+    -- 2 = Both
+    Config.currencyType = 2 -- Default: 2
+
     valentine = {
         shopName = "Valentine Wagons", -- Name of Shop on Menu
         promptName = "Valentine Wagons", -- Text Below the Prompt Button
         blipAllowed = true, -- Turns Blips On / Off
         blipName = "Valentine Wagons", -- Name of the Blip on the Map
         blipSprite = 1012165077, -- wagon wheel
-        blipColorOpen = "BLIP_MODIFIER_MP_COLOR_32", -- Shop Open - Default: White - Blip Colors Shown Below
-        blipColorClosed = "BLIP_MODIFIER_MP_COLOR_10", -- Shop Closed - Default: Red - Blip Colors Shown Below
-        blipColorJob = "BLIP_MODIFIER_MP_COLOR_23", -- Shop Job Locked - Default: Yellow - Blip Colors Shown Below
+        blipColorOpen = "WHITE", -- Shop Open - Default: White - Blip Colors Shown Below
+        blipColorClosed = "RED", -- Shop Closed - Default: Red - Blip Colors Shown Below
+        blipColorJob = "YELLOW_ORANGE", -- Shop Job Locked - Default: Yellow - Blip Colors Shown Below
         npc = {x = -383.46, y = 792.91, z = 115.81, h = 14.37}, -- Location for NPC and Shop
         spawn = {x = -392.81, y = 800.65, z = 115.86, h = 266.14}, -- Wagon Spawn and Return Positions
         wagonCam = {x = -391.07, y = 794.49, z = 115.94}, -- Camera Location to View Wagon When In-Menu
@@ -45,54 +55,101 @@ Settings can be changed in the `config.lua` file. Here is an example of one shop
         shopClose = 21, -- Shop Close Time / 24 Hour Clock
         wagons = { -- Change ONLY These Values: wagonType, label, cashPrice, goldPrice and sellPrice
             {
-                wagonType = "Buggies",
-                ["buggy01"] = { label = "buggy01", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
-                ["buggy02"] = { label = "buggy02", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
-                ["buggy03"] = { label = "buggy03", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
-                ["cart02"]  = { label = "cart02",  cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                name = "Buggies",
+                types = {
+                    ["buggy01"] = { label = "buggy01", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                    ["buggy02"] = { label = "buggy02", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                    ["buggy03"] = { label = "buggy03", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                    ["cart02"]  = { label = "cart02",  cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                }
             },
             {
-                wagonType = "Coaches",
-                ["coach3"] = { label = "coach3", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
-                ["coach4"] = { label = "coach4", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
-                ["coach5"] = { label = "coach5", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
-                ["coach6"] = { label = "coach6", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                name = "Coaches",
+                types = {
+                    ["coach3"] = { label = "coach3", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                    ["coach4"] = { label = "coach4", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                    ["coach5"] = { label = "coach5", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                    ["coach6"] = { label = "coach6", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                }
             },
             {
-                wagonType = "Carts",
-                ["cart01"] = { label = "cart01", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
-                ["cart03"] = { label = "cart03", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
-                ["cart04"] = { label = "cart04", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
-                ["cart06"] = { label = "cart06", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
-                ["cart07"] = { label = "cart07", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
-                ["cart08"] = { label = "cart08", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                name = "Carts",
+                types = {
+                    ["cart01"] = { label = "cart01", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                    ["cart03"] = { label = "cart03", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                    ["cart04"] = { label = "cart04", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                    ["cart06"] = { label = "cart06", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                    ["cart07"] = { label = "cart07", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                    ["cart08"] = { label = "cart08", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                }
             },
             {
-                wagonType = "Wagons",
-                ["huntercart01"]      = { label = "huntercart01",      cashPrice = 1, goldPrice = 1, sellPrice = 1 },
-                ["supplywagon"]       = { label = "supplywagon",       cashPrice = 1, goldPrice = 1, sellPrice = 1 },
-                ["wagon02x"]          = { label = "wagon02x",          cashPrice = 1, goldPrice = 1, sellPrice = 1 },
-                ["wagon03x"]          = { label = "wagon03x",          cashPrice = 1, goldPrice = 1, sellPrice = 1 },
-                ["wagon04x"]          = { label = "wagon04x",          cashPrice = 1, goldPrice = 1, sellPrice = 1 },
-                ["wagon05x"]          = { label = "wagon05x",          cashPrice = 1, goldPrice = 1, sellPrice = 1 },
-                ["wagon06x"]          = { label = "wagon06x",          cashPrice = 1, goldPrice = 1, sellPrice = 1 },
-                ["chuckwagon000x"]    = { label = "chuckwagon000x",    cashPrice = 1, goldPrice = 1, sellPrice = 1 },
-                ["chuckwagon002x"]    = { label = "chuckwagon002x",    cashPrice = 1, goldPrice = 1, sellPrice = 1 },
-                ["wagontraveller01x"] = { label = "wagontraveller01x", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                name = "Wagons",
+                types = {
+                    ["huntercart01"]      = { label = "huntercart01",      cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                    ["supplywagon"]       = { label = "supplywagon",       cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                    ["wagon02x"]          = { label = "wagon02x",          cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                    ["wagon03x"]          = { label = "wagon03x",          cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                    ["wagon04x"]          = { label = "wagon04x",          cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                    ["wagon05x"]          = { label = "wagon05x",          cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                    ["wagon06x"]          = { label = "wagon06x",          cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                    ["chuckwagon000x"]    = { label = "chuckwagon000x",    cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                    ["chuckwagon002x"]    = { label = "chuckwagon002x",    cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                    ["wagontraveller01x"] = { label = "wagontraveller01x", cashPrice = 1, goldPrice = 1, sellPrice = 1 },
+                }
             }
         }
     },
+
+
+    Config.BlipColors = {
+        LIGHT_BLUE    = "BLIP_MODIFIER_MP_COLOR_1",
+        DARK_RED      = "BLIP_MODIFIER_MP_COLOR_2",
+        PURPLE        = "BLIP_MODIFIER_MP_COLOR_3",
+        ORANGE        = "BLIP_MODIFIER_MP_COLOR_4",
+        TEAL          = "BLIP_MODIFIER_MP_COLOR_5",
+        LIGHT_YELLOW  = "BLIP_MODIFIER_MP_COLOR_6",
+        PINK          = "BLIP_MODIFIER_MP_COLOR_7",
+        GREEN         = "BLIP_MODIFIER_MP_COLOR_8",
+        DARK_TEAL     = "BLIP_MODIFIER_MP_COLOR_9",
+        RED           = "BLIP_MODIFIER_MP_COLOR_10",
+        LIGHT_GREEN   = "BLIP_MODIFIER_MP_COLOR_11",
+        TEAL2         = "BLIP_MODIFIER_MP_COLOR_12",
+        BLUE          = "BLIP_MODIFIER_MP_COLOR_13",
+        DARK_PUPLE    = "BLIP_MODIFIER_MP_COLOR_14",
+        DARK_PINK     = "BLIP_MODIFIER_MP_COLOR_15",
+        DARK_DARK_RED = "BLIP_MODIFIER_MP_COLOR_16",
+        GRAY          = "BLIP_MODIFIER_MP_COLOR_17",
+        PINKISH       = "BLIP_MODIFIER_MP_COLOR_18",
+        YELLOW_GREEN  = "BLIP_MODIFIER_MP_COLOR_19",
+        DARK_GREEN    = "BLIP_MODIFIER_MP_COLOR_20",
+        BRIGHT_BLUE   = "BLIP_MODIFIER_MP_COLOR_21",
+        BRIGHT_PURPLE = "BLIP_MODIFIER_MP_COLOR_22",
+        YELLOW_ORANGE = "BLIP_MODIFIER_MP_COLOR_23",
+        BLUE2         = "BLIP_MODIFIER_MP_COLOR_24",
+        TEAL3         = "BLIP_MODIFIER_MP_COLOR_25",
+        TAN           = "BLIP_MODIFIER_MP_COLOR_26",
+        OFF_WHITE     = "BLIP_MODIFIER_MP_COLOR_27",
+        LIGHT_YELLOW2 = "BLIP_MODIFIER_MP_COLOR_28",
+        LIGHT_PINK    = "BLIP_MODIFIER_MP_COLOR_29",
+        LIGHT_RED     = "BLIP_MODIFIER_MP_COLOR_30",
+        LIGHT_YELLOW3 = "BLIP_MODIFIER_MP_COLOR_31",
+        WHITE         = "BLIP_MODIFIER_MP_COLOR_32"
+    }
 ```
 
 #### Dependencies
+
 - [vorp_core](https://github.com/VORPCORE/vorp-core-lua)
 - [vorp_inventory](https://github.com/VORPCORE/vorp_inventory-lua)
 
 #### Installation
+
 - Ensure that the dependancies are added and started
-- Add `oss_wagons` folder to your resources folder
-- Add `ensure oss_wagons` to your `resources.cfg`
+- Add `bcc-wagons` folder to your resources folder
+- Add `ensure bcc-wagons` to your `resources.cfg`
 - Run the included database file `wagons.sql`
 
 #### GitHub
-- https://github.com/JusCampin/oss_wagons
+
+- https://github.com/JusCampin/bcc-wagons
--- server/server.lua
+++ server/server.lua
@@ -8,133 +8,131 @@ end)
 VORPInv = exports.vorp_inventory:vorp_inventoryApi()
 
 -- Buy New Wagons
-RegisterServerEvent('oss_wagons:BuyWagon')
-AddEventHandler('oss_wagons:BuyWagon', function(data)
+RegisterNetEvent('bcc-wagons:BuyWagon', function(data)
     local _source = source
     local Character = VORPcore.getUser(_source).getUsedCharacter
     local identifier = Character.identifier
     local charid = Character.charIdentifier
     local maxWagons = Config.maxWagons
 
-    MySQL.Async.fetchAll('SELECT * FROM player_wagons WHERE identifier = ? AND charid = ?', {identifier, charid},
-    function(wagons)
-        if #wagons >= maxWagons then
-            VORPcore.NotifyRightTip(_source, _U("wagonLimit") .. maxWagons .. _U("wagons"), 5000)
-            TriggerClientEvent('oss_wagons:WagonMenu', _source)
-            return
-        end
-        if data.IsCash then
-            local charCash = Character.money
-            local cashPrice = data.Cash
-
-            if charCash >= cashPrice then
-                Character.removeCurrency(0, cashPrice)
-            else
-                VORPcore.NotifyRightTip(_source, _U("shortCash"), 5000)
-                TriggerClientEvent('oss_wagons:WagonMenu', _source)
+    MySQL.Async.fetchAll('SELECT * FROM player_wagons WHERE identifier = ? AND charid = ?', { identifier, charid },
+        function(wagons)
+            if #wagons >= maxWagons then
+                VORPcore.NotifyRightTip(_source, _U("wagonLimit") .. maxWagons .. _U("wagons"), 5000)
+                TriggerClientEvent('bcc-wagons:WagonMenu', _source)
                 return
             end
-        else
-            local charGold = Character.gold
-            local goldPrice = data.Gold
+            if data.IsCash then
+                local charCash = Character.money
+                local cashPrice = data.Cash
 
-            if charGold >= goldPrice then
-                Character.removeCurrency(1, goldPrice)
+                if charCash >= cashPrice then
+                    Character.removeCurrency(0, cashPrice)
+                else
+                    VORPcore.NotifyRightTip(_source, _U("shortCash"), 5000)
+                    TriggerClientEvent('bcc-wagons:WagonMenu', _source)
+                    return
+                end
             else
-                VORPcore.NotifyRightTip(_source, _U("shortGold"), 5000)
-                TriggerClientEvent('oss_wagons:WagonMenu', _source)
-                return
+                local charGold = Character.gold
+                local goldPrice = data.Gold
+
+                if charGold >= goldPrice then
+                    Character.removeCurrency(1, goldPrice)
+                else
+                    VORPcore.NotifyRightTip(_source, _U("shortGold"), 5000)
+                    TriggerClientEvent('bcc-wagons:WagonMenu', _source)
+                    return
+                end
             end
-        end
-        local rename = false
-        TriggerClientEvent('oss_wagons:SetWagonName', _source, data, rename)
-    end)
+            local rename = false
+            TriggerClientEvent('bcc-wagons:SetWagonName', _source, data, rename)
+        end)
 end)
 
 -- Save New Wagon Purchase to Database
-RegisterServerEvent('oss_wagons:SaveNewWagon')
-AddEventHandler('oss_wagons:SaveNewWagon', function(data, name)
+RegisterNetEvent('bcc-wagons:SaveNewWagon', function(data, name)
     local _source = source
     local Character = VORPcore.getUser(_source).getUsedCharacter
     local identifier = Character.identifier
     local charid = Character.charIdentifier
 
-    MySQL.Async.execute('INSERT INTO player_wagons (identifier, charid, name, model) VALUES (?, ?, ?, ?)', {identifier, charid, name, data.ModelW},
+    MySQL.Async.execute('INSERT INTO player_wagons (identifier, charid, name, model) VALUES (?, ?, ?, ?)',
+        { identifier, charid, name, data.ModelW },
         function(done)
-    end)
+        end)
 end)
 
 -- Rename Owned Wagons
-RegisterServerEvent('oss_wagons:UpdateWagonName')
-AddEventHandler('oss_wagons:UpdateWagonName', function(data, name)
-
-    MySQL.Async.execute('UPDATE player_wagons SET name = ? WHERE id = ?', {name, data.WagonId},
-    function(done)
-    end)
+RegisterNetEvent('bcc-wagons:UpdateWagonName', function(data, name)
+    MySQL.Async.execute('UPDATE player_wagons SET name = ? WHERE id = ?', { name, data.WagonId },
+        function(done)
+        end)
 end)
 
-RegisterServerEvent('oss_wagons:SelectWagon')
-AddEventHandler('oss_wagons:SelectWagon', function(data)
+RegisterNetEvent('bcc-wagons:SelectWagon', function(data)
     local _source = source
     local Character = VORPcore.getUser(_source).getUsedCharacter
     local identifier = Character.identifier
     local charid = Character.charIdentifier
     local id = tonumber(data.WagonId)
 
-    MySQL.Async.fetchAll('SELECT * FROM player_wagons WHERE identifier = ? AND charid = ?', {identifier, charid},
-    function(wagon)
-        for i = 1, #wagon do
-            MySQL.Async.execute('UPDATE player_wagons SET selected = ? WHERE identifier = ? AND charid = ? AND id = ?', {0, identifier, charid, wagon[i].id},
-            function(done)
-                if wagon[i].id == id then
-                    MySQL.Async.execute('UPDATE player_wagons SET selected = ? WHERE identifier = ? AND charid = ? AND id = ?', {1, identifier, charid, id},
+    MySQL.Async.fetchAll('SELECT * FROM player_wagons WHERE identifier = ? AND charid = ?', { identifier, charid },
+        function(wagon)
+            for i = 1, #wagon do
+                MySQL.Async.execute(
+                    'UPDATE player_wagons SET selected = ? WHERE identifier = ? AND charid = ? AND id = ?',
+                    { 0, identifier, charid, wagon[i].id },
                     function(done)
+                        if wagon[i].id == id then
+                            MySQL.Async.execute(
+                                'UPDATE player_wagons SET selected = ? WHERE identifier = ? AND charid = ? AND id = ?',
+                                { 1, identifier, charid, id },
+                                function(done)
+                                end)
+                        end
                     end)
-                end
-            end)
-        end
-    end)
+            end
+        end)
 end)
 
 -- Get Selected Player Owned Wagon
-RegisterServerEvent('oss_wagons:GetSelectedWagon')
-AddEventHandler('oss_wagons:GetSelectedWagon', function()
+RegisterNetEvent('bcc-wagons:GetSelectedWagon', function()
     local _source = source
     local Character = VORPcore.getUser(_source).getUsedCharacter
     local identifier = Character.identifier
     local charid = Character.charIdentifier
-    MySQL.Async.fetchAll('SELECT * FROM player_wagons WHERE identifier = ? AND charid = ?', {identifier, charid},
-    function(wagons)
-        if #wagons ~= 0 then
-            for i = 1, #wagons do
-                if wagons[i].selected == 1 then
-                    local menuSpawn = false
-                    TriggerClientEvent('oss_wagons:SpawnWagon', _source, wagons[i].model, wagons[i].name, menuSpawn, wagons[i].id)
+    MySQL.Async.fetchAll('SELECT * FROM player_wagons WHERE identifier = ? AND charid = ?', { identifier, charid },
+        function(wagons)
+            if #wagons ~= 0 then
+                for i = 1, #wagons do
+                    if wagons[i].selected == 1 then
+                        local menuSpawn = false
+                        TriggerClientEvent('bcc-wagons:SpawnWagon', _source, wagons[i].model, wagons[i].name, menuSpawn,
+                            wagons[i].id)
+                    end
                 end
+            else
+                VORPcore.NotifyRightTip(_source, _U("noOwnedWagons"), 5000)
             end
-        else
-            VORPcore.NotifyRightTip(_source, _U("noOwnedWagons"), 5000)
-        end
-    end)
+        end)
 end)
 
 -- Get Player Owned Wagons
-RegisterServerEvent('oss_wagons:GetMyWagons')
-AddEventHandler('oss_wagons:GetMyWagons', function()
+RegisterNetEvent('bcc-wagons:GetMyWagons', function()
     local _source = source
     local Character = VORPcore.getUser(_source).getUsedCharacter
     local identifier = Character.identifier
     local charid = Character.charIdentifier
 
-    MySQL.Async.fetchAll('SELECT * FROM player_wagons WHERE identifier = ? AND charid = ?', {identifier, charid},
-    function(wagons)
-        TriggerClientEvent('oss_wagons:WagonsData', _source, wagons)
-    end)
+    MySQL.Async.fetchAll('SELECT * FROM player_wagons WHERE identifier = ? AND charid = ?', { identifier, charid },
+        function(wagons)
+            TriggerClientEvent('bcc-wagons:WagonsData', _source, wagons)
+        end)
 end)
 
 -- Sell Player Owned Wagons
-RegisterServerEvent('oss_wagons:SellWagon')
-AddEventHandler('oss_wagons:SellWagon', function(data, shopId)
+RegisterNetEvent('bcc-wagons:SellWagon', function(data, shopId)
     local _source = source
     local Character = VORPcore.getUser(_source).getUsedCharacter
     local identifier = Character.identifier
@@ -142,36 +140,35 @@ AddEventHandler('oss_wagons:SellWagon', function(data, shopId)
     local modelWagon = nil
     local wagonId = tonumber(data.WagonId)
 
-    MySQL.Async.fetchAll('SELECT * FROM player_wagons WHERE identifier = ? AND charid = ?', {identifier, charid},
-    function(wagons)
-        for i = 1, #wagons do
-            if tonumber(wagons[i].id) == wagonId then
-                modelWagon = wagons[i].model
-                MySQL.Async.execute('DELETE FROM player_wagons WHERE identifier = ? AND charid = ? AND id = ?', {identifier, charid, wagonId},
-                function(done)
-                    for _,wagonModels in pairs(Config.wagonShops[shopId].wagons) do
-                        for model,wagonConfig in pairs(wagonModels) do
-                            if model ~= "wagonType" then
-                                if model == modelWagon then
-                                    local sellPrice = wagonConfig.sellPrice
-                                    Character.addCurrency(0, sellPrice)
-                                    VORPcore.NotifyRightTip(_source, _U("soldWagon") .. data.WagonName .. _U("frcash") .. sellPrice, 5000)
+    MySQL.Async.fetchAll('SELECT * FROM player_wagons WHERE identifier = ? AND charid = ?', { identifier, charid },
+        function(wagons)
+            for i = 1, #wagons do
+                if tonumber(wagons[i].id) == wagonId then
+                    modelWagon = wagons[i].model
+                    MySQL.Async.execute('DELETE FROM player_wagons WHERE identifier = ? AND charid = ? AND id = ?',
+                        { identifier, charid, wagonId },
+                        function(done)
+                            for _, wagonModels in pairs(Config.wagonShops[shopId].wagons) do
+                                for model, wagonConfig in pairs(wagonModels["types"]) do
+                                    if model == modelWagon then
+                                        local sellPrice = wagonConfig.sellPrice
+                                        Character.addCurrency(0, sellPrice)
+                                        VORPcore.NotifyRightTip(_source,
+                                            _U("soldWagon") .. data.WagonName .. _U("frcash") .. sellPrice, 5000)
+                                    end
                                 end
                             end
-                        end
-                    end
-                end)
+                        end)
+                end
             end
-        end
-        TriggerClientEvent('oss_wagons:WagonMenu', _source)
-    end)
+            TriggerClientEvent('bcc-wagons:WagonMenu', _source)
+        end)
 end)
 
 -- Register Wagon Inventory
-RegisterServerEvent('oss_wagons:RegisterInventory')
-AddEventHandler('oss_wagons:RegisterInventory', function(id, wagonModel, shopId)
-    for _,wagonModels in pairs(Config.wagonShops[shopId].wagons) do
-        for model,wagonConfig in pairs(wagonModels) do
+RegisterNetEvent('bcc-wagons:RegisterInventory', function(id, wagonModel, shopId)
+    for _, wagonModels in pairs(Config.wagonShops[shopId].wagons) do
+        for model, wagonConfig in pairs(wagonModels) do
             if model ~= "wagonType" then
                 if model == wagonModel then
                     VORPInv.registerInventory("wagon_" .. tostring(id), _U("wagonInv"), tonumber(wagonConfig.invLimit))
@@ -182,18 +179,16 @@ AddEventHandler('oss_wagons:RegisterInventory', function(id, wagonModel, shopId)
 end)
 
 -- Open Wagon Inventory
-RegisterServerEvent('oss_wagons:OpenInventory')
-AddEventHandler('oss_wagons:OpenInventory', function(id)
+RegisterNetEvent('bcc-wagons:OpenInventory', function(id)
     local _source = source
     VORPInv.OpenInv(_source, "wagon_" .. tostring(id))
 end)
 
 -- Check Player Job and Job Grade
-RegisterServerEvent('oss_wagons:getPlayerJob')
-AddEventHandler('oss_wagons:getPlayerJob', function()
+RegisterNetEvent('bcc-wagons:getPlayerJob', function()
     local _source = source
     local Character = VORPcore.getUser(_source).getUsedCharacter
     local CharacterJob = Character.job
     local CharacterGrade = Character.jobGrade
-    TriggerClientEvent('oss_wagons:sendPlayerJob', _source, CharacterJob, CharacterGrade)
+    TriggerClientEvent('bcc-wagons:sendPlayerJob', _source, CharacterJob, CharacterGrade)
 end)

3a25b4729608d0bd887db91cb0e8ad5400626e92
Apollyon <37031311+JusCampin@users.noreply.github.com>
2023-07-27 22:19:12 -0700

    update languages
    - added dutch translation by eva

---
+++ languages/nl_lang.lua
@@ -0,0 +1,24 @@
+---------------------------------- ENGLISH -------------------------------------------
+Locales["nl_lang"] = {
+
+    shopPrompt = "Menu",
+    returnPrompt = "Lever Wagen in",
+    closed = " ~o~: ~e~SLUITEN",
+    to = ":00~q~ to ~o~",
+    hundred = ":00",
+    hours = " is open vanaf ~o~",
+    shortGold = "Je hebt niet voldoende goud!",
+    shortCash = "Je hebt niet genoeg geld!",
+    wagonReturned = "is ~o~ingeleverd",
+    noWagonReturn = "Je hebt geen wagen om in te leveren!",
+    noOwnedWagons = "Je hebt geen wagens die je beheert!",
+    needJob = "Je hebt hiervoor een baan nodig: ~o~",
+    wagonLimit = "Je kan een maximale aantal hebben van ~o~",
+    wagons = "~q~ wagens!",
+    soldWagon = "Je verkocht ~o~",
+    frcash = "~q~ voor ~t6~$",
+    nameWagon = "Geef je wagen een naam:",
+    wagonInv = "Wagen Inventory",
+    tooClose = "Je bent te dichtbij om je wagen te roepen!",
+    noWagon = "Je hebt geen wagen buiten staan!"
+}

c3afe418eb0a07848ea5a249476939eb3607dd7c
Apollyon <37031311+JusCampin@users.noreply.github.com>
2023-07-27 22:20:41 -0700

    Update server.lua
    - update db queries
    - housekeeping

--- server/server.lua
+++ server/server.lua
@@ -13,31 +13,29 @@ RegisterNetEvent('bcc-wagons:BuyWagon', function(data)
     local charid = Character.charIdentifier
     local maxWagons = Config.maxWagons
 
-    MySQL.Async.fetchAll('SELECT * FROM player_wagons WHERE identifier = ? AND charid = ?', { identifier, charid },
-    function(wagons)
-        if #wagons >= maxWagons then
-            VORPcore.NotifyRightTip(src, _U('wagonLimit') .. maxWagons .. _U('wagons'), 5000)
+    local wagons = MySQL.query.await('SELECT * FROM player_wagons WHERE identifier = ? AND charid = ?', { identifier, charid })
+    if #wagons >= maxWagons then
+        VORPcore.NotifyRightTip(src, _U('wagonLimit') .. maxWagons .. _U('wagons'), 4000)
+        TriggerClientEvent('bcc-wagons:WagonMenu', src)
+        return
+    end
+    if data.IsCash then
+        if Character.money >= data.Cash then
+            TriggerClientEvent('bcc-wagons:SetWagonName', src, data, false)
+        else
+            VORPcore.NotifyRightTip(src, _U('shortCash'), 4000)
             TriggerClientEvent('bcc-wagons:WagonMenu', src)
             return
         end
-        if data.IsCash then
-            if Character.money >= data.Cash then
-                TriggerClientEvent('bcc-wagons:SetWagonName', src, data, false)
-            else
-                VORPcore.NotifyRightTip(src, _U('shortCash'), 5000)
-                TriggerClientEvent('bcc-wagons:WagonMenu', src)
-                return
-            end
+    else
+        if Character.gold >= data.Gold then
+            TriggerClientEvent('bcc-wagons:SetWagonName', src, data, false)
         else
-            if Character.gold >= data.Gold then
-                TriggerClientEvent('bcc-wagons:SetWagonName', src, data, false)
-            else
-                VORPcore.NotifyRightTip(src, _U('shortGold'), 5000)
-                TriggerClientEvent('bcc-wagons:WagonMenu', src)
-                return
-            end
+            VORPcore.NotifyRightTip(src, _U('shortGold'), 4000)
+            TriggerClientEvent('bcc-wagons:WagonMenu', src)
+            return
         end
-    end)
+    end
 end)
 
 -- Save New Wagon Purchase to Database
@@ -47,24 +45,21 @@ RegisterNetEvent('bcc-wagons:SaveNewWagon', function(data, name)
     local identifier = Character.identifier
     local charid = Character.charIdentifier
 
-    MySQL.Async.execute('INSERT INTO player_wagons (identifier, charid, name, model) VALUES (?, ?, ?, ?)', { identifier, charid, name, data.ModelW },
-    function(done)
-        if data.IsCash then
-            Character.removeCurrency(0, data.Cash)
-        else
-            Character.removeCurrency(1, data.Gold)
-        end
-        TriggerClientEvent('bcc-wagons:WagonMenu', src)
-    end)
+    MySQL.query.await('INSERT INTO player_wagons (identifier, charid, name, model) VALUES (?, ?, ?, ?)', { identifier, charid, name, data.ModelW })
+    if data.IsCash then
+        Character.removeCurrency(0, data.Cash)
+    else
+        Character.removeCurrency(1, data.Gold)
+    end
+    TriggerClientEvent('bcc-wagons:WagonMenu', src)
 end)
 
 -- Rename Owned Wagons
 RegisterNetEvent('bcc-wagons:UpdateWagonName', function(data, name)
     local src = source
-    MySQL.Async.execute('UPDATE player_wagons SET name = ? WHERE id = ?', { name, data.WagonId },
-    function(done)
-        TriggerClientEvent('bcc-wagons:WagonMenu', src)
-    end)
+
+    MySQL.query.await('UPDATE player_wagons SET name = ? WHERE id = ?', { name, data.WagonId })
+    TriggerClientEvent('bcc-wagons:WagonMenu', src)
 end)
 
 RegisterNetEvent('bcc-wagons:SelectWagon', function(data)
@@ -74,19 +69,13 @@ RegisterNetEvent('bcc-wagons:SelectWagon', function(data)
     local charid = Character.charIdentifier
     local id = tonumber(data.WagonId)
 
-    MySQL.Async.fetchAll('SELECT * FROM player_wagons WHERE identifier = ? AND charid = ?', { identifier, charid },
-    function(wagon)
-        for i = 1, #wagon do
-            MySQL.Async.execute('UPDATE player_wagons SET selected = ? WHERE identifier = ? AND charid = ? AND id = ?', { 0, identifier, charid, wagon[i].id },
-            function(done)
-                if wagon[i].id == id then
-                    MySQL.Async.execute('UPDATE player_wagons SET selected = ? WHERE identifier = ? AND charid = ? AND id = ?', { 1, identifier, charid, id },
-                    function(done)
-                    end)
-                end
-            end)
+    local wagon = MySQL.query.await('SELECT * FROM player_wagons WHERE identifier = ? AND charid = ?', { identifier, charid })
+    for i = 1, #wagon do
+        MySQL.query.await('UPDATE player_wagons SET selected = ? WHERE identifier = ? AND charid = ? AND id = ?', { 0, identifier, charid, wagon[i].id })
+        if wagon[i].id == id then
+            MySQL.query.await('UPDATE player_wagons SET selected = ? WHERE identifier = ? AND charid = ? AND id = ?', { 1, identifier, charid, id })
         end
-    end)
+    end
 end)
 
 -- Get Selected Player Owned Wagon
@@ -95,18 +84,17 @@ RegisterNetEvent('bcc-wagons:GetSelectedWagon', function()
     local Character = VORPcore.getUser(src).getUsedCharacter
     local identifier = Character.identifier
     local charid = Character.charIdentifier
-    MySQL.Async.fetchAll('SELECT * FROM player_wagons WHERE identifier = ? AND charid = ?', { identifier, charid },
-    function(wagons)
-        if #wagons ~= 0 then
-            for i = 1, #wagons do
-                if wagons[i].selected == 1 then
-                    TriggerClientEvent('bcc-wagons:SpawnWagon', src, wagons[i].model, wagons[i].name, false, wagons[i].id)
-                end
+
+    local wagons = MySQL.query.await('SELECT * FROM player_wagons WHERE identifier = ? AND charid = ?', { identifier, charid })
+    if #wagons ~= 0 then
+        for i = 1, #wagons do
+            if wagons[i].selected == 1 then
+                TriggerClientEvent('bcc-wagons:SpawnWagon', src, wagons[i].model, wagons[i].name, false, wagons[i].id)
             end
-        else
-            VORPcore.NotifyRightTip(src, _U('noOwnedWagons'), 5000)
         end
-    end)
+    else
+        VORPcore.NotifyRightTip(src, _U('noOwnedWagons'), 4000)
+    end
 end)
 
 -- Get Player Owned Wagons
@@ -116,10 +104,8 @@ RegisterNetEvent('bcc-wagons:GetMyWagons', function()
     local identifier = Character.identifier
     local charid = Character.charIdentifier
 
-    MySQL.Async.fetchAll('SELECT * FROM player_wagons WHERE identifier = ? AND charid = ?', { identifier, charid },
-    function(wagons)
-        TriggerClientEvent('bcc-wagons:WagonsData', src, wagons)
-    end)
+    local wagons = MySQL.query.await('SELECT * FROM player_wagons WHERE identifier = ? AND charid = ?', { identifier, charid })
+    TriggerClientEvent('bcc-wagons:WagonsData', src, wagons)
 end)
 
 -- Sell Player Owned Wagons
@@ -131,27 +117,23 @@ RegisterNetEvent('bcc-wagons:SellWagon', function(data, shopId)
     local modelWagon = nil
     local wagonId = tonumber(data.WagonId)
 
-    MySQL.Async.fetchAll('SELECT * FROM player_wagons WHERE identifier = ? AND charid = ?', { identifier, charid },
-    function(wagons)
-        for i = 1, #wagons do
-            if tonumber(wagons[i].id) == wagonId then
-                modelWagon = wagons[i].model
-                MySQL.Async.execute('DELETE FROM player_wagons WHERE identifier = ? AND charid = ? AND id = ?', { identifier, charid, wagonId },
-                function(done)
-                    for _, wagonModels in pairs(Config.shops[shopId].wagons) do
-                        for model, wagonConfig in pairs(wagonModels['types']) do
-                            if model == modelWagon then
-                                local sellPrice = (Config.sellPrice * wagonConfig.cashPrice)
-                                Character.addCurrency(0, sellPrice)
-                                VORPcore.NotifyRightTip(src, _U('soldWagon') .. data.WagonName .. _U('frcash') .. sellPrice, 5000)
-                            end
-                        end
-                    end
-                end)
+    local wagons = MySQL.query.await('SELECT * FROM player_wagons WHERE identifier = ? AND charid = ?', { identifier, charid })
+    for i = 1, #wagons do
+        if tonumber(wagons[i].id) == wagonId then
+            modelWagon = wagons[i].model
+            MySQL.query.await('DELETE FROM player_wagons WHERE identifier = ? AND charid = ? AND id = ?', { identifier, charid, wagonId })
+        end
+    end
+    for _, wagonModels in pairs(Config.shops[shopId].wagons) do
+        for model, wagonConfig in pairs(wagonModels['types']) do
+            if model == modelWagon then
+                local sellPrice = (Config.sellPrice * wagonConfig.cashPrice)
+                Character.addCurrency(0, sellPrice)
+                VORPcore.NotifyRightTip(src, _U('soldWagon') .. data.WagonName .. _U('frcash') .. sellPrice, 4000)
             end
         end
-        TriggerClientEvent('bcc-wagons:WagonMenu', src)
-    end)
+    end
+    TriggerClientEvent('bcc-wagons:WagonMenu', src)
 end)
 
 -- Register Wagon Inventory
@@ -181,17 +163,11 @@ VORPcore.addRpcCallback('CheckPlayerJob', function(source, cb, shop)
             if playerJob == job then
                 if tonumber(jobGrade) >= tonumber(Config.shops[shop].jobGrade) then
                     cb(true)
-                else
-                    VORPcore.NotifyRightTip(src, _U('needJobGrade'), 4000)
-                    cb(false)
+                    return
                 end
-            else
-                VORPcore.NotifyRightTip(src, _U('needJob'), 4000)
-                cb(false)
             end
         end
-    else
-        VORPcore.NotifyRightTip(src, _U('needJob'), 4000)
-        cb(false)
     end
+    VORPcore.NotifyRightTip(src, _U('needJob'), 4000)
+    cb(false)
 end)

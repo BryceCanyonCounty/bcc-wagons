a3681bbe32c90ba98e48eb2399f6f65fa94e7504
Raoul <50175388+HariboInc@users.noreply.github.com>
2023-07-29 21:56:44 +0200

    Dutch translations

---
+++ languages/nl_lang.lua
@@ -0,0 +1,24 @@
+---------------------------------- DUTCH -------------------------------------------
+Locales["nl_lang"] = {
+
+    shopPrompt = "Menu",
+    returnPrompt = "Koets terugbrengen",
+    closed = " ~o~: ~e~GESLOTEN",
+    to = ":00~q~ tot ~o~",
+    hundred = ":00",
+    hours = " is open vanaf ~o~",
+    shortGold = "Je hebt niet genoeg goud",
+    shortCash = "Je hebt niet genoeg geld",
+    wagonReturned = "Koets is teruggebracht!",
+    noWagonReturn = "Je hebt geen koets om terug te brengen",
+    noOwnedWagons = "Je bent geen eigenaar van een koets",
+    needJob = "Je hebt niet het juiste beroep voor deze koets!",
+    wagonLimit = "Je kan een maximaal ~o~",
+    wagons = "~q~ koetsen!",
+    soldWagon = "Je verkocht ~o~",
+    frcash = "~q~ voor ~t6~$",
+    nameWagon = "Geef je koets een naam:",
+    wagonInv = "Opslag kist",
+    tooClose = "Je bent te dichtbij om je koets op te roepen",
+    noWagon = "Je hebt geen koets uit de stal genomen"
+}

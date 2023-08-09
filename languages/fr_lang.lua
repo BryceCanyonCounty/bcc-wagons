d501da57fdee10dfbc43a7033d70137044a19b2a
Apollyon <37031311+JusCampin@users.noreply.github.com>
2023-08-08 19:07:18 -0700

    Create fr_lang.lua
    - French translation by Elzetia

---
+++ languages/fr_lang.lua
@@ -0,0 +1,24 @@
+---------------------------------- FRANCAIS -------------------------------------------
+Locales["fr_lang"] = {
+
+    shopPrompt = "Menu",
+    returnPrompt = "Renvoyer le chariot",
+    closed = " ~o~: ~e~FERMÉ",
+    to = ":00~q~ à ~o~",
+    hundred = ":00",
+    hours = " est ouvert de ~o~",
+    shortGold = "Vous n'avez pas assez d'or",
+    shortCash = "Vous n'avez pas assez d'argent",
+    wagonReturned = "Le chariot a été renvoyé !",
+    noWagonReturn = "Vous n'avez pas de chariot à renvoyer",
+    noOwnedWagons = "Vous ne possédez pas de chariot",
+    needJob = "Vous n'avez pas l'emploi requis !",
+    wagonLimit = "Vous pouvez avoir au maximum ~o~",
+    wagons = "~q~ chariots !",
+    soldWagon = "Vous avez vendu ~o~",
+    frcash = "~q~ pour ~t6~$",
+    nameWagon = "Nom de votre chariot :",
+    wagonInv = "Coffre de stockage",
+    tooClose = "Vous êtes trop près de votre chariot pour l'appeler",
+    noWagon = "Vous n'avez pas de chariot"
+}

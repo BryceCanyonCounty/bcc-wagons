32dfecefe8ccc1fc69eddd2d7cb43ba1b7406e1d
Apollyon <37031311+JusCampin@users.noreply.github.com>
2024-10-18 15:20:38 -0700

    Update wagons.sql

--- wagons.sql
+++ wagons.sql
@@ -7,3 +7,18 @@ CREATE TABLE IF NOT EXISTS `player_wagons` (
   `model` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `player_wagons` ADD COLUMN IF NOT EXISTS (`condition` INT(11) NOT NULL DEFAULT 100);
+
+INSERT INTO
+    `items`(`item`, `label`, `limit`, `can_remove`, `type`, `usable`, `desc`)
+VALUES
+    ('bcc_repair_hammer', 'Repair Hammer', 1, 1, 'item_standard', 1, 'Tool used for repairs.')
+ON DUPLICATE KEY UPDATE
+    `item` = VALUES(`item`),
+    `label` = VALUES(`label`),
+    `limit` = VALUES(`limit`),
+    `can_remove` = VALUES(`can_remove`),
+    `type` = VALUES(`type`),
+    `usable` = VALUES(`usable`),
+    `desc` = VALUES(`desc`);
